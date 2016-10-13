af4dc03319ea0fbc84634e72f8c684c5edce516b
Ngos, Simon Pascal <simon.pascal.ngos@accenture.com>
2016-10-13 12:55:24 +0400

    init project

---
+++ src/main/java/org/simon/pascal/DemoLiquibaseApplication.java
@@ -0,0 +1,12 @@
+package org.simon.pascal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLiquibaseApplication.class, args);
+	}
+}
